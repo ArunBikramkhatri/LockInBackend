21fcba86cd3978a56b1573baa177af6c86a62e3c
arun_khatri <arun@callbreak.com>
2026-02-18 20:52:11 +0545

    feat: create project

---
+++ src/main/java/com/nura/lockin/LockInApplication.java
@@ -0,0 +1,13 @@
+package com.nura.lockin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nura/lockin/LockInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nura.lockin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
